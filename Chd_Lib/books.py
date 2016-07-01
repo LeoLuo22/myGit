e69c5428d1a5c3df417ab83c300f9dcbec0dfe63
Leo Luo <leoluo22@gmail.com>
2016-07-01 23:23:56 +0800

    功能实现，但速度较慢，考虑多线程

--- Chd_Lib/books.py
+++ Chd_Lib/books.py
@@ -14,24 +14,43 @@ class Books():
         self.__header = header
         self.filename = filename
 
-    def generate_payload(self):
+    def generate_payload(self, begin, end):
         payload = collections.defaultdict()
-        value = "0000198282"
+        make_up = "0000000000"
+        values = []
+        for i in range(begin, end):
+            value = i
+            value = str(value)
+            if len(value) != 10:
+                distance = 10 - len(value)
+                value = make_up[0:distance] + value
+                values.append(value)
+            else:
+                values.append(value)
+        #value = "0000198288"
         key  = "marc_no"
-        payload[key] = value
+        payload[key] = values
         return payload
 
-    def get_filename_and_url(self):
+    def get_filename_and_url(self, payload):
         result = []
-        payload = self.generate_payload()
+        #payload = self.generate_payload(211,234)
         filename_session = requests.Session()
         r = filename_session.get(BASE_URL, params=payload)
         base_soup = r.content.decode('utf-8')
         soup = BeautifulSoup(base_soup, 'lxml')
         test  = soup.find('dd')
-        title = test.a.string
+        try:
+            title = test.a.string
+        except AttributeError as err:
+            print("不存在!")
+            return 0
         test.a.decompose()
-        name  = test.string.replace("/", "_")
+        try:
+            name  = test.string.replace("/", "_")
+        except AttributeError as err:
+            print(err)
+            return 0
         filename = title + name
         result.append(filename)
         douban_url = soup.find_all('a',attrs={'target':'_blank'})
@@ -43,34 +62,53 @@ class Books():
         d = requests.get(douban)
         d_soup = d.content.decode('utf-8')
         douban_soup = BeautifulSoup(d_soup, 'lxml')
-        pic_url = douban_soup.find('a', attrs={'class':'nbg'})['href']
+        try:
+            pic_url = douban_soup.find('a', attrs={'class':'nbg'})['href']
+        except TypeError as err:
+            print("不存在!")
+            return 0
         result.append(pic_url)
         return result
 
     def get_img(self,result):
         print(result)
-        douban = result[1]
+        try:
+            douban = result[1]
+        except TypeError as err:
+            print(err)
+            return 0
         filename = result[0]
         douban_session = requests.Session()
         d = douban_session.get(douban)
         content = d.content
         now_path = os.path.abspath('.')
         save_path  = now_path + "/book/"
-        filename = "{0}{1}".format(filename,".jpg")
+        filename = "{0}{1}{2}".format(save_path, filename,".jpg")
         try:
             with open(filename, 'wb') as fh:
                 fh.write(content)
-        except FileNotFoundError as err:
-            print(err)
+        except (FileNotFoundError,OSError) as err:
+            return 0
 
+def real_payload(payload_dict):
+    for i in range(0,len(payload_dict["marc_no"])):
+        yield {"marc_no":str(payload_dict["marc_no"][i])}
 def main():
+    payload_dict = collections.OrderedDict()
+    range_begin = input("请输入起始的数字: ")
+    range_end = input("请输入结束的数字: ")
+    range_begin = int(range_begin)
+    range_end = int(range_end)
     book = Books()
-    result = book.get_filename_and_url()
-    fina = book.get_img(result)
-    cwd = os.getcwd()  # Get the current working directory (cwd)
-    files = os.listdir(cwd)  # Get all the files in that directory
-    print("Files in '%s': %s" % (cwd, files))
-    print()
+    payload_dict = book.generate_payload(range_begin, range_end)
+    a = real_payload(payload_dict)
+    for i in range(range_begin, range_end):
+        payload = next(a)
+        print(payload)
+        result = book.get_filename_and_url(payload)
+        fina = book.get_img(result)
+    #result = book.get_filename_and_url()
+    #fina = book.get_img(result)
 
 
 if __name__ == "__main__":
