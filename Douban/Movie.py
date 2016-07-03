e1979af59fd8a0a4294c8fce4da556e5d633911f
Leo Luo <leoluo22@gmail.com>
2016-07-03 14:53:02 +0800

    ...

--- Douban/Movie.py
+++ Douban/Movie.py
@@ -1,16 +1,32 @@
 # -*- coding:utf-8 -*-
 __author__ = "Leo Luo"
 
+from bs4 import BeautifulSoup
 import requests
 
-session = requests.Session()
-session.get("https://movie.douban.com/")
-login  = "https://accounts.douban.com/login"
-data = {'source':'movie',
-'redir':'https://movie.douban.com/',
-'form_email':'leoluo22@gmail.com',
-'form_password':'vampire.Leo123',
-'login':'登录'}
+BASE_URL = "https://movie.douban.com/"
+LOGIN_URL = "https://accounts.douban.com/login"
+LOGIN_DATA = { 'source':'movie',
+                                'redir':'https://movie.douban.com/',
+                                'form_email':'*******************',
+                                'form_password':'*********************',
+                                'login':'登录'}
 
-r = session.post(login, data=data)
-print(r.text)
+session = requests.Session()
+session.get(BASE_URL)
+
+class Douban(object):
+    def __init__(self):
+        super().__init__()
+
+    def login(self):
+        r = session.post(LOGIN_URL, data=LOGIN_DATA)
+        print(r.request.headers)
+        print(r.text)
+
+def main():
+    d = Douban()
+    d.login()
+
+if __name__ == "__main__":
+    main()
