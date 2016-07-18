941668564d704a0c609e40eea9c74f8d559373bf
Leo Luo <leoluo22@gmail.com>
2016-07-18 23:04:29 +0800

    Half done captcha

--- iCHD/Portal.py
+++ iCHD/Portal.py
@@ -17,6 +17,8 @@ TO_DO_URL = "http://portal.chd.edu.cn/pnull.portal?rar=&.pmn=view&.ia=false&acti
 #captchaResponse:z8dc
 session = requests.Session()
 
+Flag = True
+
 class Chd(object):
 
     def __init__(self, POST_DATA=None):
@@ -41,18 +43,34 @@ class Chd(object):
         self.__POST_DATA = value
 
     """**********************生成要发送的数据************************"""
-    def create_post_data(self, username, password):
-        r = session.get(BASE_URL)
-        base = r.content.decode('utf-8')
-        base_soup = BeautifulSoup(base,'lxml')
-        lt = base_soup.find('input',attrs={'type':'hidden', 'name':'lt'})['value']
-        self.__POST_DATA['lt'] = lt
-        self.__POST_DATA['username'] = username
-        self.__POST_DATA['password'] = password
+    def create_post_data(self, username, password, flag):
+        if flag == "no_captcha":
+            r = session.get(BASE_URL)
+            base = r.content.decode('utf-8')
+            base_soup = BeautifulSoup(base,'lxml')
+            lt = base_soup.find('input',attrs={'type':'hidden', 'name':'lt'})['value']#时间戳
+            self.__POST_DATA['lt'] = lt
+            self.__POST_DATA['username'] = username
+            self.__POST_DATA['password'] = password
+        elif flag == "captcha":
+            r = session.get(BASE_URL)
+            base = r.content.decode('utf-8')
+            base_soup = BeautifulSoup(base,'lxml')
+            lt = base_soup.find('input',attrs={'type':'hidden', 'name':'lt'})['value']
+            payload = collections.OrderedDict()
+            payload['username'] = username
+            payload['_'] = lt
+            self.get_captcha(payload)
+            captcha = get_input("请输入验证码: ", "captcha")
+            self.__POST_DATA['captcha'] = captcha
+            self.__POST_DATA['lt'] = lt
+            self.__POST_DATA['username'] = username
+            self.__POST_DATA['password'] = password
+
         #print(self.POST_DATA)
 
-    def get_captcha(self):
-        r = session.get("http://ids.chd.edu.cn/authserver/captcha.html")
+    def get_captcha(self, payload):
+        r = session.get("http://ids.chd.edu.cn/authserver/captcha.html", params=payload)
         captcha = r.content
         with open('captcha.png', 'wb') as fh:
             fh.write(captcha)
@@ -61,7 +79,13 @@ class Chd(object):
 
     def login(self):
         contents = {}
-        login = session.post(BASE_URL, params=self.POST_DATA, headers=HEAD)
+        try:
+            login = session.post(BASE_URL, params=self.POST_DATA, headers=HEAD)
+        except requests.exceptions.TooManyRedirects as err:
+            print("出现错误，请重新登录")
+            Flag = False
+            main()
+            return 0
         login = login.content.decode('utf-8')
         contents["index"] = login
         """ FUCK
@@ -119,6 +143,7 @@ class Parse(object):
         except TypeError as err:
             print("出现错误, 请重新登录")
             main()
+            return 0
         user_img_para = str(user_img_para).replace("amp;","")
         user_img_url = "http://portal.chd.edu.cn/" + user_img_para
         return user_img_url
@@ -129,7 +154,12 @@ class Parse(object):
         #print(user_info_soup)
         _money = user_info_soup.find_all('span', attrs={'class':'msgTitle1 desc'})
         print(_money)
-        name = user_info_soup.find('span', attrs={'style':'margin-bottom:5px;display:block;'}).string.split()
+        try:
+            name = user_info_soup.find('span', attrs={'style':'margin-bottom:5px;display:block;'}).string.split()
+        except AttributeError as err:
+            print("出现错误, 请重新登录")
+            main()
+            return 0
         department = user_info_soup.find('span',attrs={'style':'display:block;'}).string.strip(whitespace)
         department = department.replace("部门：","")
         self.__user_info["name"] = name[0]
@@ -171,24 +201,61 @@ def get_input(msg,  _type):
             break
         return seq_num
 
+    elif _type == "captcha":
+        while True:
+            captcha = input(msg)
+            if len(captcha) != 4 or len(captcha) == 0:
+                print("输入有误，请重新输入")
+            else:
+                return captcha
+
+def init():
+    Flag = not Flag
+
+
 def main():
-    user = Chd()
-    username = get_input("请输入用户名: ","username")
-    password = get_input("请输入密码: ","password")
-    user.create_post_data(username, password)
-    #user.get_captcha()
-    contents  = user.login()
-    parser = Parse(contents['index'])
-    user_img_url = parser.get_user_img_url()
-    #print(user_img_url)
-    option = input("请选择是否显示头像: (y/n)")
-    if option == 'y' or option == "Y":
-        user.show_user_img(user_img_url)
+    if Flag == True:
+        user = Chd()
+        username = get_input("请输入用户名: ","username")
+        password = get_input("请输入密码: ","password")
+        user.create_post_data(username, password, "no_captcha")
+        #user.get_captcha()
+        contents  = user.login()
+        parser = Parse(contents['index'])
+        #user_img_url = parser.get_user_img_url()
+        #print(user_img_url)
+        #option = input("请选择是否显示头像: (y/n)")
+        """
+        if option == 'y' or option == "Y":
+            user.show_user_img(user_img_url)
+        else:
+            pass
+            """
+        parser.get_user_info()
+        other = Parse(contents["to_do_info"])
+        other.get_other_info()
+        print("你当前校园卡的余额为: {0}".format(other.user_info['money']))
     else:
-        pass
-    parser.get_user_info()
-    other = Parse(contents["to_do_info"])
-    other.get_other_info()
-    print("你当前校园卡的余额为: {0}".format(other.user_info['money']))
+        user = Chd()
+        username = get_input("请输入用户名: ","username")
+        password = get_input("请输入密码: ","password")
+        user.create_post_data(username, password, "captcha")
+        #user.get_captcha()
+        contents  = user.login()
+        parser = Parse(contents['index'])
+        #user_img_url = parser.get_user_img_url()
+        #print(user_img_url)
+        #option = input("请选择是否显示头像: (y/n)")
+        """
+        if option == 'y' or option == "Y":
+            user.show_user_img(user_img_url)
+        else:
+            pass
+            """
+        parser.get_user_info()
+        other = Parse(contents["to_do_info"])
+        other.get_other_info()
+        print("你当前校园卡的余额为: {0}".format(other.user_info['money']))
+
 if __name__ == "__main__":
     main()
