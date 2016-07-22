ab9bc1c349e93b9eaa41609cee9519f7072eed0b
Leo Luo <leoluo22@gmail.com>
2016-07-22 14:39:09 +0800

    First GUI

---
+++ GUI/SimpleGUI.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+from tkinter import *
+
+window = Tk()
+label = Label(window, text = "Welcome")
+button = Button(window, text = "Click")
+label.pack()
+button.pack()
+
+window.mainloop()
