7110cbf2ff74c57a98037df3616baf19ed9aef18
Leo Luo <leoluo22@gmail.com>
2016-07-28 13:32:15 +0800

    DRY

--- Python_Web开发测试驱动方法/superlists/lists/views.py
+++ Python_Web开发测试驱动方法/superlists/lists/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     #return HttpResponse('<html><title>To-Do lists </title></html>')
-    return render(request, 'home.html')
+    """
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
+    """
+    if request.method == 'GET':
+        return render(request, 'home.html')
+    else:
+        return render(request, 'home.html', {'new_item_text' : request.POST.get('item_text'), })
