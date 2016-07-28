cb0826c1fab754379f4bc3875bf6879c10a3d4be
Leo Luo <leoluo22@gmail.com>
2016-07-28 15:29:38 +0800

    make functional_tests an app, not user LiveServerTestCase

--- Python_Web开发测试驱动方法/superlists/functional_tests/tests.py
+++ Python_Web开发测试驱动方法/superlists/functional_tests/tests.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -19,18 +20,18 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.brower.get('http://localhost:8000')
+        self.brower.get('localhost:8000')
         self.assertIn('To-Do', self.brower.title)
         header_text = self.brower.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         inputbox = self.brower.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        inputbox = self.brower.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test')
-
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
