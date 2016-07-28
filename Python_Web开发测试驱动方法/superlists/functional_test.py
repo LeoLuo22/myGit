2240f405526fe652722ad4a0e17faf746a6c5a69
Leo Luo <leoluo22@gmail.com>
2016-07-28 13:42:55 +0800

    hello

--- Python_Web开发测试驱动方法/superlists/functional_test.py
+++ Python_Web开发测试驱动方法/superlists/functional_test.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.brower.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.brower.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.brower.get('http://localhost:8000')
         self.assertIn('To-Do', self.brower.title)
@@ -23,10 +28,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.brower.find_element_by_id('id_lists_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.fail('Finish the test')
 
 if __name__ == "__main__":
