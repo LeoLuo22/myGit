070428bd54741e597c484feb679563541f789c39
Leo Luo <leoluo22@gmail.com>
2016-07-28 15:46:22 +0800

    test

--- Python_Web开发测试驱动方法/superlists/functional_tests/tests.py
+++ Python_Web开发测试驱动方法/superlists/functional_tests/tests.py
@@ -28,10 +28,36 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.brower.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         inputbox = self.brower.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.fail('Finish the test')
+
+        #self.fail('Finish the test')
+        self.brower.quit()
+        self.brower = webdriver.Firefox()
+
+        self.brower.get('localhost:8000')
+        page_text = self.brower.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        inputbox = self.brower.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.brower.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #This page contains the list without Edith
+        page_text = self.brower.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        #They're both happy and go to bed
